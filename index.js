1b3fb1cfe274c9541af99877806a3c5359cd95af
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-01-06 19:04:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.querySelector("main#main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Zelie is the champion";
+document.body.append(newHeader);
